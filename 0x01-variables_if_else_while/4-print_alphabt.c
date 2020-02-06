71f707b09d6049b42ebc7e687cedc3b07702d45b
julian <juliancano.dev@gmail.com>
2020-02-06 18:20:52 -0500

    c_beggining

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 97 ; a < 123 ; a++)
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+	}
+putchar('\n');
+}
