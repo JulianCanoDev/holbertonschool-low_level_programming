3cf0e0ea42e2421d39fad5a4d104cc6f3d349d52
julian <juliancano.dev@gmail.com>
2020-02-11 19:24:43 -0500

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ * @num_one: int variable
+ * @num_two: int variable
+ * Return: Always 0 (Success)
+ */
+
+int add(int num_one, int num_two)
+{
+	return (num_one + num_two);
+}
