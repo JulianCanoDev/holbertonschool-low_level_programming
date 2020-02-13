be7927e4b6dd0f3bd3a92bf51b74ec4c21012eed
julian <juliancano.dev@gmail.com>
2020-02-13 15:09:13 -0500

    More

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Entry point
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	for (num = 48 ; num <= 57 ; num++)
+	{
+		_putchar(num);
+	}
+_putchar('\n');
+}
