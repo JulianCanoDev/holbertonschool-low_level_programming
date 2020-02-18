c82aaf6be62c024b09b74427c7d3cfe99137c992
julian <juliancano.dev@gmail.com>
2020-02-18 11:19:52 -0500

    Pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,16 @@
 
 /**
  * _puts - Entry pointer
- * @*str: char variable
+ * @str: char variable
  * Return: Always 0 (Success).
  */
 void _puts(char *str)
 {
-	
+	int i = 0;
+
+	for (i = 0 ; str[i] != 0 ; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
 }
