46b4293549580d9827083d86b86931ba388f6c68
julian <juliancano.dev@gmail.com>
2020-02-18 18:48:53 -0500

    Pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,16 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int rev = 0;
+	int temp = 0;
 
 	for (i = 0 ; s[i] != 0 ; i++)
 	{
 		rev++;
 	}
-	for (i = rev - 1 ; s[i] > 0 ; i--)
+	for (i = rev - 1 ; i > 4 ; i--)
 	{
-		
+		temp = s[i];
+		s[i] = s[rev -1 - i];
+		s[rev -1  - i] = temp;
 	}
 }
