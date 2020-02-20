21d589e69553f397a4462b7b0c22e3bbd8665883
julian <juliancano.dev@gmail.com>
2020-02-20 15:41:59 -0500

    More pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
-//#include <string.h>
 
 /**
- * _strncat
+ * _strncat - Entry point
  * @dest: char pointer
  * @src: char pointer
  * @n: int variable
@@ -13,16 +12,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int len1 = 0;
 
-	for(i = 0 ; dest[i] ; i++)
+	for (i = 0 ; dest[i] ; i++)
 	{
 		len1++;
 	}
-	char* ptr = dest + len1;
+	char *ptr = dest + len1;
 
 	while (*src != '\0' && n--)
 	{
 		*ptr++ = *src++;
 	}
 	*ptr = '\0';
-	return dest;
+	return (dest);
 }
