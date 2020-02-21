0c73dc5ffcc53b4af5fd9e6a6ac0dbe7fdd75ddf
julian <juliancano.dev@gmail.com>
2020-02-21 18:28:47 -0500

    More pointers

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,6 +8,19 @@
 char *rot13(char *n)
 {
 	int i = 0;
+	int j = 0;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	
+	for (i = 0 ; n[i] != '\0' ; i++)
+	{
+		for (j = 0 ; letters[j] != '\0' ; j++)
+		{
+			if (n[i] == letters[j])
+			{
+				n[i] = rot13[j];
+			}
+		}
+	}
+return (n);
 }
