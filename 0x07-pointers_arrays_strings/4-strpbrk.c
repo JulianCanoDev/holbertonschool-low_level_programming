75e251da8c3424839df0b9f779e010e8c844d2fc
julian <juliancano.dev@gmail.com>
2020-02-24 20:12:39 -0500

    Arrays

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,22 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * _strpbrk - Entry point
- * @s: char pointer
- * @accept: char pointer
- * Return: Always 0 (Success).
- */
+  * _strpbrk - function that searched a string for any of a set of bytes
+  * @s: string
+  * @accept: string comparing string to
+  * Return: Null if nothing matches
+  */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (NULL);
 }
