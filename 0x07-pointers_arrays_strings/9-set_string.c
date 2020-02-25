c09ab065aea7526d639adf4229572066794238d5
julian <juliancano.dev@gmail.com>
2020-02-24 21:13:06 -0500

    Arrays

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - Entry point
+ * @s: pointer to pointer
+ * @to: pointed
+ * Return: Always 0 (Success).
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
