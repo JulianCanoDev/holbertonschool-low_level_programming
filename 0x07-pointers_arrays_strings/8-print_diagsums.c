2967e9ec602d21d8436f38eb1cc1b4af23d10ad9
julian <juliancano.dev@gmail.com>
2020-02-24 20:46:21 -0500

    Arrays

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - Entry point
+ * @s: char pointer to pointer
+ * @to: char pointer
+ * Return: Always 0 (Success).
+ */
+void set_string(char **s, char *to)
+{
+}
