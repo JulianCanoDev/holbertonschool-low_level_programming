21219af19e9ea0c2ce96c1b6f0f5e42030ebb82e
julian <juliancano.dev@gmail.com>
2020-03-01 14:48:45 -0500

    bdfbdf

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.c>
+
+/**
+ * main - Entry point
+ * @argc: int variable
+ * @argv: char array pointer
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
