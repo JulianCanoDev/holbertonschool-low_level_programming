da90102adbfb21d935709820d68807ead39b4c0e
julian <juliancano.dev@gmail.com>
2020-03-09 18:43:39 -0500

    Macros

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type
+ * @d: Struct pointer
+ * @name: Char pointer
+ * @age: Float variable
+ * @owner: Char pointer
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		d -> name = name;
+		d -> age = age;
+		d -> owner = owner;
+	}
+}
