4407da88d15933c88dddfb2aaef56ae38874ad3b
julian <juliancano.dev@gmail.com>
2020-03-09 20:14:00 -0500

    Structures

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: char pointer array
+ * @age: float variable
+ * @owner: char pointer array
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	create malloc n dogs;
+}
