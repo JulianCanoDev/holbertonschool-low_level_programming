1ce3bb736ede159e55502664fd6d8a5fa3ecbb87
julian <juliancano.dev@gmail.com>
2020-03-10 14:27:39 -0500

    macros

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,33 +15,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *copy_name, *copy_owner;
 	int x, name_len = 0, owner_len = 0;
 
-	new_name = malloc(sizeof(dog_t));
-	if (name == NULL)
-		return (NULL);
-	if (name == NULL || age <= 0 || owner == NULL)
-	{
-		free(new_name);
-		return (NULL);
-	}
-
 	for (x = 0; name[x] != '\0'; x++)
 		name_len++;
 
 	for (x = 0; owner[x] != '\0'; x++)
 		owner_len++;
-
-	copy_name = malloc(sizeof(char) * (name_len + 1));
-	if (copy_name == NULL)
-		return (NULL);
-
-	copy_owner = malloc(sizeof(char) * (owner_len + 1));
-	if (copy_owner == NULL)
-		return (NULL);
-
-	new_name = malloc(sizeof(dog_t));
+	copy_name = malloc(name_len + 1);
 	if (name == NULL)
 		return (NULL);
-
+	copy_owner = malloc(owner_len + 1);
+	if (name == NULL)
+	{
+		free(copy_name);
+		return (NULL);
+	}
+	new_name = malloc(sizeof(dog_t));
+	if (new_name == NULL)
+	{
+		free(copy_name);
+		free(copy_owner);
+		return (NULL);
+	}
 	for (x = 0; x <= name_len; x++)
 		copy_name[x] = name[x];
 
