d09c775d95a9bc17a97b02e5659603fe833c2ab0
julian <juliancano.dev@gmail.com>
2020-03-09 20:38:44 -0500

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+int get_len(int i);
+char *str_cpy(char *dest, char *src);
 
 /**
  * new_dog - function that creates a new dog.
@@ -9,5 +11,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	create malloc n dogs;
+	dog_t *new_name;
+	char *copy_name, *copy_owner;
+	int x, name_len = 0, owner_len = 0;
+
+	new_name = malloc(sizeof(dog_t));
+	if (name == NULL)
+		return (NULL);
+	if (name == NULL || age <= 0 || owner == NULL)
+	{
+		free(new_name);
+		return (NULL);
+	}
+
+	for (x = 0; name[x] != '\0'; x++)
+		name_len++;
+
+	for (x = 0; owner[x] != '\0'; x++)
+		owner_len++;
+
+	copy_name = malloc(sizeof(char) * (name_len + 1));
+	if (copy_name == NULL)
+		return (NULL);
+
+	copy_owner = malloc(sizeof(char) * (owner_len + 1));
+	if (copy_owner == NULL)
+		return (NULL);
+
+	for (x = 0; x <= name_len; x++)
+		copy_name[x] = name[x];
+
+	for (x = 0; x <= owner_len; x++)
+		copy_owner[x] = owner[x];
+
+	new_name->name = copy_name;
+	new_name->owner = copy_owner;
+	new_name->age = age;
+	return (new_name);
 }
