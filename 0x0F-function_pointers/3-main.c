fc4e46eb1ff8f856d494c0fcefddc302a1d458e9
julian <juliancano.dev@gmail.com>
2020-03-11 17:13:46 -0500

    Pointer to function

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ * @argc: int variable
+ * @argv: char array pointer
+ * Return: No 0
+ */
+int main(int argc, char *argv[])
+{
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - add a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplicate a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	else
+	{
+		return (a / b);
+	}
+}
+
+/**
+ * op_mod - module a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	else
+	{
+		return (a % b);
+	}
+}
