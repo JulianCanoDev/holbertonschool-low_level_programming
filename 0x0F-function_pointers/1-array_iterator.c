7af96b8409daf4a23f2adc26207fd05bf0a1e6cf
julian <juliancano.dev@gmail.com>
2020-03-11 15:21:25 -0500

    Pointer to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function
+ * @array: char
+ * @size: ??
+ * @action: function pointer
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0 ; i < size ; ++i)
+	{
+		(*action)(*(array + i));
+	}
+}
