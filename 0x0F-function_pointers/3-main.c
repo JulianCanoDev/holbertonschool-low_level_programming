0dc320f974c3e73809829a4d8accc8490a3e23d2
julian <juliancano.dev@gmail.com>
2020-03-11 19:13:46 -0500

    Pointer to functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int number1 = 0;
-	int number2 = 0;
+	int num1 = 0;
+	int num2 = 0;
 	int result = 0;
 
 	if (argc != 4)
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 	number1 = atoi(argv[1]);
 	number2 = atoi(argv[3]);
-	result = get_op_func(argv[2])(number1, number2);
+	result = get_op_func(argv[2])(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
