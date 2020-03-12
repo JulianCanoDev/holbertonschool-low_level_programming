600a709a0234207e9eb48b87f15789934b7e0ebe
julian <juliancano.dev@gmail.com>
2020-03-11 19:11:19 -0500

    Pointer to functions

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#include "3-calc.h"
 
 /**
  * main - Entry point
@@ -8,4 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
+	int number1 = 0;
+	int number2 = 0;
+	int result = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[3]);
+	result = get_op_func(argv[2])(number1, number2);
+	printf("%d\n", result);
+	return (0);
 }
