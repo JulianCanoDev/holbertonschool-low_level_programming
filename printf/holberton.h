b706c3cfb67ea73637bb25eceadb7f764ca28dc5
julian <juliancano.dev@gmail.com>
2020-03-14 09:57:22 -0500

    functions

--- printf/holberton.h
+++ printf/holberton.h
@@ -6,5 +6,9 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+int _printf(const char *format, ...);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void _puts(char *str);
 
 #endif
