9f9888e69a5e9812e1b3260c7f23bb76b42169aa
Julian Cano dev <juliancano.dev@gmail.com>
2020-03-19 15:47:28 -0500

    0-print.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * size_t print_list - print a list
- * h: list
+ * print_list - print a list
+ * @h: list
  * Return: Printed list
  */
 size_t print_list(const list_t *h)
