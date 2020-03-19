0ef7eca669e8d6fd2cbc2f321b2ef86c79ca05e5
Julian Cano dev <juliancano.dev@gmail.com>
2020-03-19 15:46:40 -0500

    0-print.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,5 +7,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	
+	unsigned long n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", h->len, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
