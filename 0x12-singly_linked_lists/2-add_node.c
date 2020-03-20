2dffd5f9c7c5cf8f0f8ae7bdbbe209622451fb20
Julian Cano dev <juliancano.dev@gmail.com>
2020-03-19 20:29:36 -0500

    0-print.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,6 +8,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	int i = 0;
+	int strlen = 0;
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
@@ -15,7 +17,12 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		strlen++;
+	}
 	new->str = strdup(str);
+	new->len = strlen;
 	new->next = *head;
 	*head = new;
 	return (new);
