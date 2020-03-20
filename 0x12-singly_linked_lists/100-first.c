e646fcff3155baad99a8532f46b5e64efa346a44
Julian Cano dev <juliancano.dev@gmail.com>
2020-03-19 23:22:01 -0500

    0-print.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * holamundo - print
+ * Return: void
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
