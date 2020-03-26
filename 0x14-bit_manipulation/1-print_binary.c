b2335618175e2043a21f993d4103af9bc8c86fd1
julian <juliancano.dev@gmail.com>
2020-03-26 13:13:16 -0500

    Bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_binary - Entry Point
+ * @n: dec input
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int count = 0;
+	int k = 0;
+	int temp = 0;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	temp = n;
+
+	while (temp != 0)
+	{
+		i++;
+		temp = temp >> 1;
+	}
+
+	for (count = i - 1; count >= 0; count--)
+	{
+		k = n >> count;
+		if (k & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
