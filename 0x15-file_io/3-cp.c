efc9877335b1571dbc1e760f7fc064a4d3acb206
julian <juliancano.dev@gmail.com>
2020-03-31 21:27:49 -0500

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,5 +7,10 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int file_from, file_to;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97));
+	filefrom = open(argv[1], O_RDONLY);
+	if (file)
 }
