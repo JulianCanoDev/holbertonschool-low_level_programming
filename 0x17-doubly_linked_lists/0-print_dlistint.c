f9ba0c5206ba16ebc3ac5d4f3ba4392e4ed27583
julian <juliancano.dev@gmail.com>
2020-05-02 09:30:57 -0500

    Task 0 rev

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_dlistint: function that prints all the elements.
+ * print_dlistint - function that prints all the elements.
  * @h: lists
  * Return: printed list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
 	while (h)
 	{
