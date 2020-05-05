3056e9066c5b9c0652083273b095d02ea35fe10e
julian <juliancano.dev@gmail.com>
2020-05-04 21:57:59 -0500

    advanced

---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+/**
+ * add - add a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substract a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplicate a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - module a and b number
+ * @a: int variable
+ * @b: int variable
+ * Return: Result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
