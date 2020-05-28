4c59f18f3dca1bc1fc47d02ebc894c09bd0bc180
julian <juliancano.dev@gmail.com>
2020-05-28 17:08:02 -0500

    Task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - djb2 function
+ * @str: const unsigned char
+ * Return: hash
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
