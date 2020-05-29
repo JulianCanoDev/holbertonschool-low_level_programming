6f4121ac1f78068320de25c028e1d1620e67c955
julian <juliancano.dev@gmail.com>
2020-05-29 14:14:01 -0500

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - function that deletes a hash table.
+ * @ht: hash table
+ * Return: deleted hash table
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int size;
