a6320adf4ef27f198b28753797e535df0a806db3
julian <juliancano.dev@gmail.com>
2020-05-29 14:08:56 -0500

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,34 @@
 #include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int size;
+	unsigned long int i;
+	hash_node_t *element;
+	hash_node_t *node_to_delete;
+
+	i = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	size = ht->size;
+	for (i = 0; i < size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			element = ht->array[i];
+			while (element != NULL)
+			{
+				free(element->key);
+				free(element->value);
+				node_to_delete = element;
+				element = element->next;
+				free(node_to_delete);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
