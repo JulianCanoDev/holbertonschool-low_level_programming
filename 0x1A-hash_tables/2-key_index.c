35b53a5138acffc7019e2b3b1ec90ed066ce9d98
julian <juliancano.dev@gmail.com>
2020-05-29 11:24:36 -0500

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,13 +8,13 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int long_hash;
+	unsigned long int long_hash;
 	unsigned long int index;
 
 	if (size == 0)
-    {
+	{
 		return (0);
-    }
+	}
 
 	long_hash = hash_djb2(key);
 	index = long_hash % size;
