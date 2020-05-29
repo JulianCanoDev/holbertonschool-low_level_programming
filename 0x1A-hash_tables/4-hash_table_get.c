6059278e07833eb619e51a8c29a97b332e7e66af
julian <juliancano.dev@gmail.com>
2020-05-29 12:57:43 -0500

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,35 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: const hash table
+ * @key: const char
+ * Return: Associate key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int size = 0;
+	unsigned long int index = 0;
+	hash_node_t *actual_node;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+	actual_node = ht->array[index];
+	if (actual_node == NULL)
+	{
+		return (NULL);
+	}
+	while (actual_node != NULL)
+	{
+		if (strcmp(actual_node->key, key) == 0)
+		{
+			return (actual_node->value);
+		}
+		actual_node = actual_node->next;
+	}
+	return (NULL);
+}
